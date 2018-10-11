1a117a2bae404a074965eaa831a21ad317866e9b
dna909 <dna909@googlemail.com>
2018-10-11 06:17:05 +0200

    commit

--- main.js
+++ main.js
@@ -98,9 +98,9 @@ function main() {
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('config Email: '    + adapter.config.eMail);
-    adapter.log.info('config pwd: '    + adapter.config.pwd);
-    adapter.log.info('config mySelect: ' + adapter.config.mySelect);
+       adapter.log.info('config test1: '    + adapter.config.test1);
+       adapter.log.info('config test1: '    + adapter.config.test2);
+       adapter.log.info('config mySelect: ' + adapter.config.mySelect);
 
     if (!adapter.config.clientID) {
         adapter.log.error('Client ID not specified!');
