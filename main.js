31c6f6eff9164f73b68e72587a05e8839ef86787
dna909 <dna909@googlemail.com>
2018-10-29 07:32:04 +0100

    commit

--- main.js
+++ main.js
@@ -120,9 +120,6 @@ function main() {
 let scope=adapter.config.scope;
 let clientID=adapter.config.clientID;
 
-//let authUrl=auth.post(scope,clientID);
-
-//adapter.log.error(authUrl);
 
 const promise = auth.post(scope,clientID); 
 promise.then(successCallback, failureCallback);
