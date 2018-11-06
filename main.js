a95b7badd945e6d42cd245f06f8fcd316ca28b88
dna909 <dna909@googlemail.com>
2018-11-06 13:06:13 +0100

    commit

--- main.js
+++ main.js
@@ -174,7 +174,7 @@ stateGet(stat).then(
             }
             );
             }else if (value == true){
-                adapter.log.error('Devicecode: '+ deviceCode);
+                adapter.log.error('Devicecode: '+ deviceCode+'    value=true');
                 //getToken();
             }
     },
