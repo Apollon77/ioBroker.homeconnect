161d33839a79a16a55d44f1b6627ae52cb1209c2
dna909 <dna909@googlemail.com>
2018-11-27 13:21:18 +0100

    test

--- main.js
+++ main.js
@@ -510,6 +510,7 @@ aktuellen Status abfragen und Datenpunkte anlegen und States setzen
 
 switch (type){
     case "Oven":
+    adapter.log.debug('CASE');
     devicesDp(deviceArray.Oven);
     break;
     
@@ -555,6 +556,7 @@ function devicesDp(deviceArrayDp){
     let deviceArrayDpLength=deviceArrayDp.length;
     let counterdeviceArrayDp=0;
 
+    devicesDpLoop();
     
     function devicesDpLoop(){
     
